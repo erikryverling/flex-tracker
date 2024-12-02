be6512a3c9ff321a266089093289df9d15aa2c05
Erik R. Yverling <erik.r@yverling.se>
2024-12-02 21:30:08 +0100

    Remove plugins block from root build.gradle.kts

--- build.gradle.kts
+++ build.gradle.kts
@@ -4,11 +4,3 @@ allprojects {
         mavenCentral()
     }
 }
-
-plugins {
-    alias(libs.plugins.android.application).apply(false)
-    alias(libs.plugins.android.kotlin).apply(false)
-    alias(libs.plugins.hilt).apply(false)
-    alias(libs.plugins.compose.compiler) apply false
-    alias(libs.plugins.ksp) apply false
-}
