b76ce276b9726cf3209a7ddbfec36a69342f70b4
Erik R. Yverling <erik.r@yverling.se>
2025-01-30 21:38:45 +0100

    Bump dependencies

--- buildSrc/src/main/java/Versions.kt
+++ buildSrc/src/main/java/Versions.kt
@@ -1,6 +1,6 @@
 object Versions {
-    const val compileSdk = 34
-    const val targetSdk = 34
+    const val compileSdk = 35
+    const val targetSdk = 35
     const val minSdk = 33
 
     const val jvmTarget = "17"
